c3acebf88da3ecd6143bde92218f1a7fad6ec1c3
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2024-05-11 16:02:08 +0200

    pagination to github api requests

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../enviroment';
 
 @Injectable({
     providedIn: 'root',
@@ -9,14 +8,14 @@ import { environment } from '../../enviroment';
 
 export class DataService {
     private apiUrl = 'https://api.github.com/search/issues';
-   
+    private pageNr = 1;
+    private repo = encodeURIComponent("repo:angular/components");
+    private per_page = 100;
     constructor(private http: HttpClient) { }
 
     getData(): Observable<any> {
-        const query = encodeURIComponent("repo:angular/components");
-       
-        const result = this.http.get(`${this.apiUrl}?q=${query}`);
-    
+       // github docs: https://docs.github.com/en/rest/reference/search#search-issues-and-pull-requests
+        const result = this.http.get(`${this.apiUrl}?q=${this.repo}&page=${this.pageNr}&per_page=${this.per_page}`);
         return result;
     }
 }
