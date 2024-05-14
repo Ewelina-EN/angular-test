55f77a62e1961cdf5eb8de3a0cf358d9e43972c9
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2024-05-14 14:00:33 +0200

    add styles and clean up

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -6,27 +6,27 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from "@angular/material/paginator";
 import { PageEvent } from "@angular/material/paginator";
 import { Sort, MatSort, MatSortModule } from "@angular/material/sort";
+import { DatePipe } from "@angular/common";
 
 @Component({
     selector: 'second-page',
     standalone: true,
     imports: [RouterLink, ButtonComponent, MatTableModule, MatPaginatorModule, MatPaginator, MatSortModule],
     templateUrl: './table.component.html',
-    styleUrls: ['./table.component.scss']
+    styleUrls: ['./table.component.scss'],
+    providers: [DatePipe]
 })
 
 export class TableComponent implements OnInit {
     dataSource = new MatTableDataSource<any>([]);
     displayedColumns: string[] = ['created_at', 'updated_at', 'title'];
-    pageSize = 10;
-    length = 0;
+    pageSize = 100;
+    length = 1;
 
     @ViewChild(MatPaginator) paginator!: MatPaginator;
     @ViewChild(MatSort) sort!: MatSort;
 
     constructor(private dataService: DataService, private router: Router, private route: ActivatedRoute) { }
-
-    isLoadingFromQueryParams = false;
 
     ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -35,9 +35,8 @@ export class TableComponent implements OnInit {
         const sortOrder = params['order'] || 'desc';
 
         this.loadData(page, sortField, sortOrder);
-        this.isLoadingFromQueryParams = false;
-    });
-}
+        });
+    }
 
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator;
@@ -45,29 +44,20 @@ export class TableComponent implements OnInit {
 
         this.sort.sortChange.subscribe((sortState: Sort) => {
         this.handleSort(sortState);
-  });
+        });
     }
 
-    loadData(page: number = 1, sortField: string = 'created_at', sortOrder: string = 'desc') {
-        if (!this.isLoadingFromQueryParams) {
+    loadData(page: number, sortField: string = 'created_at', sortOrder: string = 'desc') {
             this.dataService.getData(page, this.pageSize).subscribe(data => {
                 this.dataSource.data = data.items;
-                console.log("dane", data);
                 this.length = data.total_count;
                 this.sort.active = sortField;
                 this.sort.direction = sortOrder as ('asc' | 'desc');
                 this.dataSource.sort = this.sort;
-                if (this.paginator) {
-                    this.paginator.pageIndex = page - 1;
-                }
             });
-        }
     }
 
     handlePageEvent(event: PageEvent) {
-        this.isLoadingFromQueryParams = true;
-        console.log("ActiveSortField:", this.sort.active);
-        console.log("SortOrder:", this.sort.direction);
         const pageIndex = event.pageIndex + 1;
         const sortField = this.sort.active;
         const sortOrder = this.sort.direction;
@@ -75,13 +65,12 @@ export class TableComponent implements OnInit {
         this.router.navigate(['/table'], {
             queryParams: { page: pageIndex, sort: sortField, order: sortOrder },
             queryParamsHandling: 'merge'
-        });
-
+        }).then(() => {
         this.loadData(pageIndex, sortField, sortOrder);
+        });;
     }
 
     handleSort(sortState: Sort) {
-        this.isLoadingFromQueryParams = true;
         const sortField = sortState.active;
         const sortOrder = sortState.direction;
         const pageIndex = this.paginator.pageIndex + 1;  // Uzyskaj aktualny indeks strony
@@ -92,5 +81,13 @@ export class TableComponent implements OnInit {
         });
 
         this.loadData(pageIndex, sortField, sortOrder);
+    }
+
+    formatDate(date: string): string {
+        const datePipe = new DatePipe('en-US');
+        const datePart = datePipe.transform(date, 'MM-dd-yy');
+        const timePart = datePipe.transform(date, 'hh:mm:ss a');
+
+    return datePart && timePart ? `${datePart}<br>${timePart.toLowerCase()}` : '';
     }
 }
